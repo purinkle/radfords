12f835b3de6a84c28240968c9cb53b81e7184e92
Robert Whittaker <cyclistsdismount@gmail.com>
2011-08-27 14:19:33 +0100

    Defined a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def title
+    base_title = "Radfords of Somerford"
+    
+    if @title.nil?
+      base_title
+    else
+      "#{@title} | #{base_title}"
+    end
+  end
 end
