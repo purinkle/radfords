78da8f6bf03af1d5f729f5a5e1e7557ae538f857
Robert Whittaker <cyclistsdismount@gmail.com>
2011-08-27 13:33:25 +0100

    Added per-page titles to the Pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def products
+    @title = "Products"
   end
   
   def outlets
+    @title = "Outlets"
   end
 
 end
