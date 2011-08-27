7ae38fb0c23c91bc3692db2b9cfbb5b0097cdd19
Robert Whittaker <cyclistsdismount@gmail.com>
2011-08-27 08:44:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radfords::Application.config.secret_token = 'dbe69f2b55f6784074b3f721d2b14d760e2407fb671f158817c555deb615cbc1f5c9070f2835fa29fc12c0da637380130bc2136b89fe20bb5ff1d57fd8c5f698'
