617e1e88e96042ea340946fc3d577a6dd86c10e3
Robert Whittaker <cyclistsdismount@gmail.com>
2011-09-07 12:24:33 +0100

    Added a migration for enforcing email uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110902113714) do
+ActiveRecord::Schema.define(:version => 20110907112311) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
