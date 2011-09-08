8f7e4247953e8c8eaa16858fdde6717fe0efce09
Robert Whittaker <cyclistsdismount@gmail.com>
2011-09-08 12:50:49 +0100

    Updated the data model to include the salt attribute.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110907115954) do
+ActiveRecord::Schema.define(:version => 20110908114855) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110907115954) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
