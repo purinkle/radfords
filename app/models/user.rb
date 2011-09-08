33e58ffa7122650f03c9dff613986e0f54b7dac1
Robert Whittaker <cyclistsdismount@gmail.com>
2011-09-08 12:54:29 +0100

    has_password? method with secure encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@
 #  updated_at :datetime
 #
 
+require 'digest'
+
 class User < ActiveRecord::Base
   attr_accessor :password
   
@@ -29,12 +31,25 @@ class User < ActiveRecord::Base
 
   before_save :encrypt_password
   
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
   private
     def encrypt_password
+      self.salt = make_salt if new_record?
       self.encrypted_password = encrypt(password)
     end
     
     def encrypt(string)
-      string
+      secure_hash("#{salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
     end
 end
