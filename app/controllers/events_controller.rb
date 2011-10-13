85751b32d5c692b8d480262fbd86b58938dde09f
Robert Whittaker <cyclistsdismount@gmail.com>
2011-10-13 14:58:00 +0100

    Added page to edit events.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,24 +3,29 @@ class EventsController < ApplicationController
     @event = Event.find( params[:id] )
     @title = @event.name
   end
-  
+
   def new
     @title = 'New Event'
     @event  = Event.new
   end
-  
+
   def create
     @title = 'New Event'
     @event = Event.new( params[:event] )
-    
+
     if @event.save
       redirect_to @event
     else
       render 'new'
     end
   end
-  
+
   def index
     @title = 'Listing events'
+  end
+
+  def edit
+    @event = Event.find( params[:id] )
+    @title = "Edit event"
   end
 end
