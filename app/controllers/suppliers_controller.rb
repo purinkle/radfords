6fb0da96e598a19c47d685c378cfcb153f19eb75
Robert Whittaker <cyclistsdismount@gmail.com>
2011-11-20 19:39:01 +0000

    Temporary commit.

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -2,4 +2,19 @@ class SuppliersController < ApplicationController
   def new
     @supplier = Supplier.new
   end
+
+  def create
+    @supplier = Supplier.new(params[:supplier])
+
+    if @supplier.save
+      redirect_to @supplier
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @supplier = Supplier.find(params[:id])
+    @title = @supplier.name
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Radfords::Application.routes.draw do
   resources :events
   resources :sessions, :only => [ :new, :create, :destroy ]
-  resources :suppliers, only: [:index, :new, :show]
+  resources :suppliers
 
   root :to => 'pages#home'
 
