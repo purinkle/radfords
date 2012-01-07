a10637b0162993c90b63970031882b4857ee7cc2
Robert Whittaker <cyclistsdismount@gmail.com>
2012-01-07 12:31:32 +0000

    Redirect www URLs to non-www URL.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_filter :ensure_domain
+
   protect_from_forgery
   
   include SessionsHelper
+
+  APP_DOMAIN = 'radfordsofsomerford.co.uk'
+
+  def ensure_domain
+    return unless Rails.env.production?
+
+    if request.env['HTTP_HOST'] != APP_DOMAIN
+      redirect_to "http://#{APP_DOMAIN}", status: 301
+    end
+  end
 end
