5b3f7ad9b21b8205f01ebd98dd97a13c8525a104
Robert Whittaker <cyclistsdismount@gmail.com>
2012-03-21 00:55:42 +0000

    Add `attr_accessible` attributes.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+  attr_accessible :title, :description, :image_url, :price
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
