f94e19d5358cb0cfe02cf70e4835000b17b17915
Robert Whittaker <cyclistsdismount@gmail.com>
2012-03-28 21:56:52 +0100

    Remove 301 redirect.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_filter :ensure_domain
-
-  protect_from_forgery
-  
   include SessionsHelper
-
-  APP_DOMAIN = 'radfordsofsomerford.co.uk'
-
-  def ensure_domain
-    return unless ENV['RACK_ENV'] == 'production'
-
-    if request.env['HTTP_HOST'] != APP_DOMAIN
-      redirect_to "http://#{APP_DOMAIN}", status: 301
-    end
-  end
+  protect_from_forgery
 end
