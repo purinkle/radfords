b6c00941d5f8434e67433f75e675946cb1d05e4c
Robert Whittaker <cyclistsdismount@gmail.com>
2012-04-03 19:59:58 +0100

    Store current basket in the session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   protect_from_forgery
+
+  private
+
+  def current_basket
+    Basket.find(session[:basket_id])
+  rescue
+    basket = Basket.create
+    session[:basket_id] = basket.id
+    basket
+  end
 end
