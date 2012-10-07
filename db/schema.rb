5cc7ade72a1d09db95ecadd8f0078850111027e6
Robert Whittaker <purinkle@Jolteon.local>
2012-10-07 21:24:31 +0100

    Add attachment photo to products.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120713205807) do
+ActiveRecord::Schema.define(:version => 20121007202315) do
 
   create_table "baskets", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -47,10 +47,14 @@ ActiveRecord::Schema.define(:version => 20120713205807) do
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price",       :precision => 8, :scale => 2
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.decimal  "price",              :precision => 8, :scale => 2
+    t.datetime "created_at",                                       :null => false
+    t.datetime "updated_at",                                       :null => false
     t.string   "slug"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "products", ["slug"], :name => "index_products_on_slug"
