845bfb1a2f20c44f55e67071acdb62266567adc6
Rob Whittaker <cyclistsdismount@gmail.com>
2013-05-09 12:51:45 +0100

    Hide secret key from version control.
    https://trello.com/c/al2k3nBp
    
    The secret key that is used to limit malicious attacks on the
    application was stored in version control, which reduces the
    effectiveness of the secret key. The key was regenerated and stored in a
    way that won't be used in version control.
    
    * Moved secret key into the environment file.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Radfords::Application.config.secret_token = 'dbe69f2b55f6784074b3f721d2b14d760e2407fb671f158817c555deb615cbc1f5c9070f2835fa29fc12c0da637380130bc2136b89fe20bb5ff1d57fd8c5f698'
+Radfords::Application.config.secret_token = ENV['SECRET_TOKEN']
