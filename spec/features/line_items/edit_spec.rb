a0c353016e14fade3981cf877e174161d027bbf6
Rob Whittaker <cyclistsdismount@gmail.com>
2014-09-21 20:33:04 +0100

    Add "decrease quantity" button
    Previously, there was no way for a customer to decrease the quantity of items in
    their basket, which meant they had to remove the item completely before adding
    the correct quantity to the basket. Added a new button to the basket that allows
    the user to decrease the quantity of an item in the basket.
    
    https://trello.com/c/TVOY3tPB

--- spec/features/line_items/edit_spec.rb
+++ spec/features/line_items/edit_spec.rb
@@ -1,12 +1,12 @@
 require "spec_helper"
 
 module Features
-  describe "destroy line item" do
+  describe "edit line item" do
     let(:user) { FactoryGirl.create :user }
 
     before { FactoryGirl.create :product }
 
-    it "removes the line item from the basket" do
+    it "increases the item's quantity" do
       visit signin_path
 
       fill_in("Email", with: user.email)
@@ -21,6 +21,24 @@ module Features
       click_button "+"
 
       expect(page).to have_content("2 ×")
+    end
+
+    it "decreases the item's quantity" do
+      visit signin_path
+
+      fill_in("Email", with: user.email)
+      fill_in("Password", with: user.password)
+
+      click_button "Sign in"
+
+      visit root_path
+
+      find("input[type=submit]").click
+
+      click_button "+"
+      click_button "-"
+
+      expect(page).to have_content("1 ×")
     end
   end
 end
