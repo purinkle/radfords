9e99e864af87ce68154ef0610ef13f37f4c6fd2f
Rob Whittaker <cyclistsdismount@gmail.com>
2015-09-04 13:48:33 +0100

    Create a Hound-compliant Unicorn configuration
    Previously, the Unicorn configuration wasn't compliant with Hound's
    guidelines, which was causing comments to be raised during code reviews.
    Updated the configuration to be compliant with Hound.
    
    * Fine-tuned the configuration in line with Heroku's recommendations
    
    https://trello.com/c/KhrKpb01

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,12 @@
+# https://devcenter.heroku.com/articles/rails-unicorn
+
 worker_processes (ENV["UNICORN_WORKERS"] || 3).to_i
-timeout (ENV["UNICORN_TIMEOUT"] || 5).to_i
+timeout (ENV["UNICORN_TIMEOUT"] || 15).to_i
 preload_app true
 
-before_fork do |server, worker|
+before_fork do |_server, _worker|
   Signal.trap "TERM" do
-    puts "Unicorn master intercepting TERM and sending myself QUIT instead"
-    Process.kill("QUIT", Process.pid)
+    Process.kill "QUIT", Process.pid
   end
 
   if defined? ActiveRecord::Base
@@ -13,12 +14,12 @@ before_fork do |server, worker|
   end
 end
 
-after_fork do |server, worker|
-  Signal.trap "TERM" do
-    puts "Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT"
-  end
-
+after_fork do |_server, _worker|
   if defined? ActiveRecord::Base
-    ActiveRecord::Base.establish_connection
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+             Rails.application.config.database_configuration[Rails.env]
+    config["reaping_frequency"] = (ENV["DB_REAPING_FREQUENCY"] || 10).to_i
+    config["pool"] = (ENV["DB_POOL"] || 2).to_i
+    ActiveRecord::Base.establish_connection(config)
   end
 end
