5fab9a9dca49040386cb776fe67b0426542b6a17
Rob Whittaker <cyclistsdismount@gmail.com>
2015-09-09 13:36:26 +0100

    Set default_url_options to ENV["HOST"] on servers
    Previously, the host for the any email assets was hard-coded into the
    environment configuration, which meant that the host was being defined
    twice. Changed the environment configurations to use the `"HOST"`
    environment setting.
    
    * Changed staging configuration to load all production settings
    * Upgraded rack-timeout and sprocket-rails
    
    https://trello.com/c/q3XKLPJd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {
-    host: "radfordsofsomerford.co.uk"
-  }
+  config.action_mailer.default_url_options = { host: 'ENV.fetch("HOST")' }
 end
