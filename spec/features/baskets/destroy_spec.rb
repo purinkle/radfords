fad1633732043fa41390cffb469cc44d2567d952
Rob Whittaker <cyclistsdismount@gmail.com>
2016-05-31 17:17:15 +0100

    Add destroy basket integration spec
    https://trello.com/c/KcVYtpKN

---
+++ spec/features/baskets/destroy_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+describe "deleting baskets" do
+  before { create_product }
+
+  it "deletes existing basket" do
+    visit root_path
+    find("input[type=submit]").click
+
+    click_button "Empty basket"
+
+    expect(page).to have_title("Your Basket")
+    expect(page).to have_content("Your basket is currently empty.")
+    expect(Basket.count).to eql(0)
+  end
+end
