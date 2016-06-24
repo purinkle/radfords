b05a70a2b5da9d2ac3f36d300ca9148b1c225a87
Rob Whittaker <cyclistsdismount@gmail.com>
2016-06-24 14:05:48 +0100

    Add test for failed update of suppliers
    https://trello.com/c/Hq81ljFy

--- spec/features/suppliers/updating_spec.rb
+++ spec/features/suppliers/updating_spec.rb
@@ -19,4 +19,31 @@ describe "updating suppliers" do
     expect(page).to have_content(supplier.telephone_number)
     expect(page).to have_content(supplier.website)
   end
+
+  context "when the attributes are invalid" do
+    it "shows the 'Edit Supplier' page" do
+      supplier = VCR.use_cassette("google maps") do
+        FactoryGirl.create(:supplier)
+      end
+
+      sign_in
+      visit edit_supplier_url(supplier)
+
+      VCR.use_cassette("google maps") do
+        fill_form_and_submit(
+          :supplier,
+          :edit,
+          address: "",
+          name: "",
+          telephone_number: "",
+        )
+      end
+
+      expect(page).to have_title("Edit Supplier")
+      expect(page).to have_content("errors prohibited this from being saved")
+      expect(page).to have_content("Address can't be blank")
+      expect(page).to have_content("Name can't be blank")
+      expect(page).to have_content("Telephone number can't be blank")
+    end
+  end
 end
