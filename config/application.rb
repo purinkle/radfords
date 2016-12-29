64d497c541d5f0757a6245e73d8057a9520153f0
Rob Whittaker <cyclistsdismount@gmail.com>
2016-12-29 12:14:52 +0000

    Remove duplicate test directory
    Before, the application included two test directories, when we only need
    Spec tests. Removed the `test` directory from the application.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,13 @@
-# frozen_string_literal: true
-require_relative "boot"
-require "rails/all"
+require_relative 'boot'
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 module Radfords
   class Application < Rails::Application
